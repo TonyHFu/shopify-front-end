365a5d4dced1cd6a1a0a2bd3bb1fb8665015def8
tofutigerz <hfu3@ualberta.ca>
2022-05-13 20:08:11 -0600

    styles footer a bit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,9 +5,17 @@ function Footer() {
 	return (
 		<footer>
 			<span>Created by Tony Fu (2022)</span>
-			<i className="fa-brands fa-github-alt"></i>
-			<i className="fa-brands fa-linkedin-in"></i>
-			<i className="fa-solid fa-globe"></i>
+			<div id="links">
+				<a href="https://github.com/TonyHFu/" target="_blank">
+					<i className="fa-brands fa-github-alt"></i>
+				</a>
+				<a href="https://www.linkedin.com/in/tonyhfu/" target="_blank">
+					<i className="fa-brands fa-linkedin-in"></i>
+				</a>
+				<a href="https://tonyfu.netlify.app/" target="_blank">
+					<i className="fa-solid fa-globe"></i>
+				</a>
+			</div>
 		</footer>
 	);
 }
